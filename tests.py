b723c24d7069a55603f1e77008ee74aecb2bbebe
svolganov <svolganov@mwnts.ru>
2024-08-27 22:47:12 +0300

    edit workflows

--- tests.py
+++ tests.py
@@ -35,14 +35,14 @@ def main():
         print(1)
         sys.exit()
 
-    item_id: int = 10
-    items = test_items(item_id)
-    if items['item_id'] == item_id:
-        print(f'test_items passed')
-    else:
-        print(f'test_items failed')
-        print(1)
-        sys.exit()
+    item_ids: list = [10, 20, 30, 40, 500, 678]
+    for item_id in item_ids:
+        items = test_items(item_id)
+        if items['item_id'] != item_id:
+            print(f'test_items failed')
+            print(1)
+            sys.exit()
+    print(f'test_items passed')
 
     connection = test_connection()
     if connection['status'] == 0:
@@ -52,29 +52,38 @@ def main():
         print(1)
         sys.exit()
 
-    table = test_table('testtable')
-    if table['status'] == 0:
-        print(f'test_table passed')
-    elif table['status'] == 1:
-        print(f'test_table failed')
-        print(1)
-        sys.exit()
+    for i in range(10):
+        table = test_table(f'testtable_{i}')
+        if table['status'] == 1:
+            print(f'test_table failed')
+            print(1)
+            sys.exit()
+    print(f'test_table passed')
 
-    insert = test_insert('testtable')
-    if insert['status'] == 0:
-        print(f'test_insert passed')
-    elif insert['status'] == 1:
-        print(f'test_insert failed')
-        print(1)
-        sys.exit()
+    for i in range(10):
+        insert = test_insert(f'testtable_{i}')
+        if insert['status'] == 1:
+            print(f'test_insert failed')
+            print(1)
+            sys.exit()
+    print(f'test_insert passed')
 
-    get = test_get('testtable')
-    if get['status'] == 0:
-        print(f'test_get passed')
-    elif get['status'] == 1:
-        print(f'test_get failed')
-        print(1)
-        sys.exit()
+    for i in range(10):
+        get = test_get(f'testtable_{i}')
+        if get['status'] == 1:
+            print(f'test_get failed')
+            print(1)
+            sys.exit()
+    print(f'test_get passed')
+
+    for i in range(10):
+        connection = test_connection()
+        if connection['status'] == 0:
+            print(f'test_connection passed')
+        elif connection['status'] == 1:
+            print(f'test_connection failed')
+            print(1)
+            sys.exit()
 
 
 if __name__ == '__main__':
